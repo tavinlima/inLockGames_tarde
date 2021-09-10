025995d5a0d31134ecd26595a01992ff8ca35110
Gustavo Lima <glima.0913@gmail.com>
2021-09-10 16:10:07 -0300

    listarTodos de jogos funcionando

--- senai.inlock.webApi/senai.inlock.webApi/Repositories/JogoRepository.cs
+++ senai.inlock.webApi/senai.inlock.webApi/Repositories/JogoRepository.cs
@@ -10,7 +10,7 @@ namespace senai.inlock.webApi.Repositories
 {
     public class JogoRepository : IJogoRepository
     {
-        private string stringConexao = @"Data Source=DESKTOP-QMET0P1\SQLEXPRESS; initial catalog=inlock_games_tarde; user Id=sa; pwd=123enzzo";
+        private string stringConexao = @"Data Source=DESKTOP-8FOKHBA\SQLEXPRESS; initial catalog=inlock_games_tarde; user Id=sa; pwd=senai@132";
         public void AtualizarJogo(JogoDomain jogoAtualizado)
         {
             using (SqlConnection con = new SqlConnection(stringConexao))
@@ -42,13 +42,18 @@ namespace senai.inlock.webApi.Repositories
 
         public List<JogoDomain> ListarTodos()
         {
-<<<<<<< HEAD
             List<JogoDomain> listaJogos = new List<JogoDomain>();
 
             using (SqlConnection con = new SqlConnection(stringConexao))
             {
 
-                string querySelectAll = "SELECT   idJogo, ISNULL(JOGOS.idEstudio), nomeEstudio, nomeJogo, descricao, dataLancamento, valor  FROM JOGOS FULL OUTER JOIN ESTUDIO ON JOGOS.idEstudio = ESTUDIO.idEstudio";
+                string querySelectAll = @"SELECT ISNULL(idJogo, 0), 
+                                          ISNULL(JOGOS.idEstudio, 0), 
+                                          ISNULL(nomeEstudio, 'Não cadastrado'), 
+                                          ISNULL(nomeJogo, 'Não cadastrado'), 
+                                          ISNULL(descricao, 'Sem descrição'), ISNULL(dataLancamento, ''), 
+                                          ISNULL(valor, 0)  FROM JOGOS 
+                                          FULL OUTER JOIN ESTUDIO ON JOGOS.idEstudio = ESTUDIO.idEstudio";
 
                 con.Open();
                 SqlDataReader rdr;
@@ -74,7 +79,7 @@ namespace senai.inlock.webApi.Repositories
                             nomeJogo = rdr[3].ToString(),
                             descricao = rdr[4].ToString(),
                             dataLancamento = Convert.ToDateTime(rdr[5]),
-                            valor = Convert.ToInt32(rdr[6])
+                            valor = Convert.ToDouble(rdr[6].ToString().Replace("R$ ", ""))
 
                         };
 
@@ -86,9 +91,6 @@ namespace senai.inlock.webApi.Repositories
             }
 
             return listaJogos;
-=======
-            throw new NotImplementedException();
->>>>>>> 089c23fe163a6625a099cdaad4e108270b37dacb
         }
     }
 }
